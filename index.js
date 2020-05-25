71028bd2c401388b4df97a5ed4211f59491fb2a3
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-25 22:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   var x = findFn();
+  console.log(x);
   for (let i = 0; i < array.length; i++) {
     if (array[i] === x) {
       return (array[i]);
