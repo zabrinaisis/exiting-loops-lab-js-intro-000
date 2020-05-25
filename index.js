00d846fd93620482e1e545612e6cb65659a3c27c
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-25 21:51:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] ==! stopValue) {
+    if (array[i] !== stopValue) {
         array[i] = changeValue;
     } else {
       break;
@@ -11,7 +11,7 @@ function breakOut(array, changeValue, stopValue) {
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] ==! skipValue) {
+    if (array[i] !== skipValue) {
       array[i] = changeValue;
     } else {
       continue;
